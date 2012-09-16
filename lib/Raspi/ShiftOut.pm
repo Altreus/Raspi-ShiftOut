5090d99cd001ad078feb97288f3d9c52c9053825
Alastair McGowan-Douglas <alastair.douglas@gmail.com>
2012-09-16 21:14:42 +0100

    Implement the boring, simple algorithm.
    Dunno when I'll find out whether this works or not.

--- lib/Raspi/ShiftOut.pm
+++ lib/Raspi/ShiftOut.pm
@@ -6,6 +6,12 @@ use warnings;
 
 use Device::BCM2835;
 
+use constant MSBFIRST => 0;
+use constant LSBFIRST => 1;
+
+use constant BIGENDIAN => 0;
+use constant LITTLEENDIAN => 1;
+
 =head1 NAME
 
 Raspi::ShiftOut - Send bits to a shift register
@@ -40,25 +46,61 @@ the usual way);
 
 =head2 MSBFIRST, LSBFIRST
 
-Specify these symbols to export them. They are always available as
-C<Raspi::ShiftOut::{M,L}SBFIRST>.
+These determine the bit order of the data. You have to specify these symbols to
+export them. They are always available as C<Raspi::ShiftOut::{M,L}SBFIRST>. 
 
 =head2 :bit_order
 
 Exports both C<MSBFIRST> and C<LSBFIRST>
 
+=head2 BIGENDIAN, LITTLEENDIAN
+
+These determine the byte order of the data. You have to specify these symbols to
+export them. They are always available as C<Raspi::ShiftOut::{BIG,LITTLE}ENDIAN>. 
+
+=head2 :byte_order
+
+Exports both C<BIGENDIAN> and C<LITTLEENDIAN>
+
 =head2 :all
 
 Exports all the things.
 
-=head1 SUBROUTINES/METHODS
+=head1 SUBROUTINES
 
-=head2 shift_out($data_pin, $clock_pin, $bit_order, $data)
+=head2 shift_out($data_pin, $clock_pin, $bit_order, $data[, $bytes, [$byte_order]])
+
+Outputs C<$data> as a sequence of C<$bytes> bytes of binary data. The first byte
+will go out first, unless C<$byte_order> is C<LITTLEENDIAN>, when it will go last.
+
+If the C<$bit_order> is C<LSBFIRST>, the bits in each byte will be reversed.
+
+The C<$data_pin> and C<$clock_pin> accept anything from Device::BCM2835 that can
+be given to L<<C<gpio_write>|Device::BCM2835>>.
+
+C<$bytes> defaults to 1, and C<$byte_order> defaults to C<BIGENDIAN>.
 
 =cut
 
 sub shift_out {
-    my ($data_pin, $clock_pin, $bit_order, $data) = @_;
+    my ($data_pin, $clock_pin, $bit_order, $data, $bytes, $endian) = @_;
+
+    $bytes ||= 1;
+    $endian ||= BIGENDIAN;
+
+    my $binary = sprintf "%0${$bytes*8}b", $data;
+
+    for ($endian == BIGENDIAN ? 0 .. $bytes : reverse 0 .. $bytes) {
+        my $byte = substr $binary, $_ * 8, 8;
+
+        $byte = reverse $byte if $bit_order == LSBFIRST;
+
+        for my $bit (split //, $byte) {
+            Device::BCM2835::gpio_write($data_pin, $bit);
+            Device::BCM2835::gpio_write($clock_pin, 1);
+            Device::BCM2835::gpio_write($clock_pin, 1);
+        }
+    }
 }
 
 =head1 AUTHOR
